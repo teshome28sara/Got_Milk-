22da5c19495d01e31f50c9ad3c2736eeb0c623fc
Jamie Otto <jmotto31@gmail.com>
2022-09-09 14:59:23 -0700

    added routes to controllers index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,2 +1,9 @@
-// collect all of the routes server.js can use 
-// router.use
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
