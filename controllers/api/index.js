b0045b3124b32f6c32ef25dce62cb85e7c61f76f
Jamie Otto <jmotto31@gmail.com>
2022-09-09 15:10:11 -0700

    added api routes to index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,11 @@
 // Gather up all the different api routes 
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const ingredientRoutes = require('./ingredientRoutes');
+const groceryRoute= require('./mealRoutes');
+
+router.use('/users', userRoutes);
+router.use('/ingredients', ingredientRoutes);
+router.use('/groceries', groceryRoute);
+
+module.exports = router;
