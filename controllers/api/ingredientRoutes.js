a122e758f9010a5caa3c15c8e7c221426c58d741
Mikell <Mikellt@live.com>
2022-09-10 05:05:42 -0700

    boiler plate

--- controllers/api/ingredientRoutes.js
+++ controllers/api/ingredientRoutes.js
@@ -1,17 +1,44 @@
 const router = require('express').Router();
 const Ingredients = require('../../models/Ingredients');
 
-//get all ingredients
+// GET all ingredients
 router.get('/', async (req, res) => {
-    // find all ingredients in the db and set the data equal to ingredientsData
     const ingredientsData = await Ingredients.findAll().catch((err) => { 
         res.json(err);
     });
     // We use map() to iterate over ingredientsData and serialize each object
     const ingredients = ingredientsData.map((ingredients) => ingredients.get({ plain: true }));
-    // We render the template, 'all', passing in ingredients, a new array of serialized objects
     res.render('all', { ingredients });
 });
 
+// POST create new ingredient
+router.post('/', async (req, res) => {
+    try {
+        const ingredientsData = await ingredients.create(req.body);
+        res.status(200).json(ingredientsData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// DELETE ingredient
+router.delete('/:id', async (req, res) => {
+    try {
+        const ingredientsData = await ingredients.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!ingredientsData) {
+            res.status(404).json({ message: 'No ingredient found with that id!' });
+            return;
+        }
+
+        res.status(200).json(ingredientsData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
