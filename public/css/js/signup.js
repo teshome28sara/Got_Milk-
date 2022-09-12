9b9b4b1bd9efd35b8c70ba63e713c01cdb2c8d1c
Jamie Otto <jmotto31@gmail.com>
2022-09-12 14:41:31 -0700

    sugnup form handlebar added

--- public/css/js/signup.js
+++ public/css/js/signup.js
@@ -1,8 +1,8 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#user-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
   
     if (username && password) {
       const response = await fetch('/api/users', {
