46fb639aabb1c6351001a2490cf03ae3617be2c9
sara teshome <tes28sar@gmail.com>
2022-09-12 20:45:43 -0700

    fixed typo

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,24 +1,23 @@
 const sequelize = require('../config/connection');
-const { User, Ingredients  } = require('../models');
+const { User, Ingredient } = require('../models');
 
 const userData = require('./userData.json');
-const ingredientsData = require('./ingredientsData.json');
-
+const ingredientData = require('./ingredientData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const user = await User.bulkCreate(userData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  const ingredients = await Ingredients.bulkCreate(ingredientsData, {
-    individualHooks: true,
-    returning: true,
-  });
-
- 
+  for (const ingredient of ingredientData) {
+    await Ingredient.create({
+      ...ingredient,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };
